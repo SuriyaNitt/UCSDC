57e4520f3a4a216b0cf0fbda798be76b611cb80b
Suriya Narayanan L <suriyanarayanan987@gmail.com>
2016-10-12 21:16:55 +0530

    Network added
    Signed-off-by: Suriya Narayanan L <suriyanarayanan987@gmail.com>

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import numpy as np
 from tqdm import tqdm
 import hickle
 import random
+from sklearn.cross_validation import train_test_split
 
 def cache_data(data, path):
     if os.path.isdir(os.path.dirname(path)):
@@ -67,17 +68,36 @@ def loadDataFromCaches(masterXCaches, rows, cols):
     progressBar.close()
     return imgs
 
-def model():
-    
+def network(rows, cols):
+    # Building Residual Network
+    net = tflearn.input_data(shape=[None, rows, cols, 3])
+    net = tflearn.conv_2d(net, 64, 3, activation='relu', bias=False)
+    # Residual blocks
+    net = tflearn.residual_bottleneck(net, 3, 16, 64)
+    net = tflearn.residual_bottleneck(net, 1, 32, 128, downsample=True)
+    net = tflearn.residual_bottleneck(net, 2, 32, 128)
+    net = tflearn.residual_bottleneck(net, 1, 64, 256, downsample=True)
+    net = tflearn.residual_bottleneck(net, 2, 64, 256)
+    net = tflearn.batch_normalization(net)
+    net = tflearn.activation(net, 'relu')
+    net = tflearn.global_avg_pool(net)
+    # Regression
+    net = tflearn.fully_connected(net, 1, activation='relu')
+    net = tflearn.regression(net, optimizer='momentum',
+                             loss='mean_square',
+                             learning_rate=0.1)
+    return net 
 
 def train():
     #masterX = load_imgs(224, 224) # Use this only if the input data is not cached
+    rows = 224
+    cols = 224
     numCaches = 31
     caches = range(31)
     caches = [c+1 for c in caches]
     random.shuffle(caches)
     masterXCaches = caches[:7]
-    masterX = loadDataFromCaches(masterXCaches, 224, 224)
+    masterX = loadDataFromCaches(masterXCaches, rows, cols)
     masterY = load_gt()
     newY = []
     for i in range(len(masterXCaches)):
@@ -88,6 +108,21 @@ def train():
         newY.extend(partY)
     print len(newY)
     print 'Loaded data'
+
+    randomState = 51
+    testSize = 0.1 * len(masterXCaches)
+    trainX, trainY, testX, testY = train_test_split(masterX, newY, test_size=testSize, random_state=randomState)
+    print trainX.shape
+    print trainY.shape
+    print testX.shape
+    print testY.shape
+
+    myNet = network(rows, cols)
+    # Training
+    model = tflearn.DNN(myNet, checkpoint_path='model_resnet',
+                        max_checkpoints=10, tensorboard_verbose=3, tensorboard_dir='./tflearn_logs')
+    model.fit(trainX, trainY, n_epoch=1, validation_set=(testX, testY),
+              show_metric=True, batch_size=32, run_id='resnet')
 
 
 if __name__ == '__main__':
